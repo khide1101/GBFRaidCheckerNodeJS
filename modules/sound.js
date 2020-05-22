a2b419043737290bd04497a5dcd295ec1f4139a0
hide <hide@example.com>
2020-05-22 21:18:08 +0900

    sound fix

--- modules/sound.js
+++ modules/sound.js
@@ -1,16 +1,16 @@
-const player = require('play-sound')(opts = {})
+const player = require('play-sound');
 const path = require('path');
 const exec = require('child_process').exec;
 
 const wavPath = path.resolve('./sound.wav').replace(/\\/g, '//');
 const winCmd = `powershell.exe -Command (New-Object Media.SoundPlayer "${wavPath}").PlaySync()`;
 
-module.exports = (volume, osPlatform) => {
+module.exports = (volume = 0.5, osPlatform) => {
     if (osPlatform === 'win32') {
         // Widows
         exec(winCmd);
     } else {
         // Mac, Linux
-        player.play('sound.wav', { afplay: ['-v', volume ? volume : 0.5 ] });
+        player(opts = {}).play('sound.wav', { afplay: ['-v', volume] });
     }
 };
